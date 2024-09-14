8a2b2df1e8427e990371d47afb4c8729e1a23da5
Prakash <jprakashchaudhary857@gmail.com>
2024-09-14 14:08:20 +0545

    deploy11

--- app/Http/Controllers/EsewaController.php
+++ app/Http/Controllers/EsewaController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class EsewaController extends Controller
 {
+    protected $menu;
     public function index()
     {
         // Return a payment page view where users can trigger the eSewa payment
@@ -19,6 +20,7 @@ class EsewaController extends Controller
         $amt=$request->input('amt');
         $tax=20;
         $menu_item=$request->input('menu_item');
+        $this->menu=$menu_item;
 
         $total=$amt+$tax;
         $unique_id=uniqid();
@@ -38,8 +40,9 @@ class EsewaController extends Controller
             'scd' => 'epay_payment', // Merchant code (test mode)
             'su'  => $success_url, // Success URL
             'fu'  => $failure_url, // Failure URL
+            // 'menu_item_id' => $menu_item
         ];
-        orderfood::create([
+             orderfood::create([
             'transaction_id'=>$unique_id,
             'user_id'=>auth()->user()->id,
             'menu_item_id'=>$menu_item,
@@ -56,7 +59,29 @@ class EsewaController extends Controller
     {
         // Handle success response from eSewa
         $refId = $request->input('refId');
-        return 'Payment Successful with Reference ID: ' . $refId;
+        $user=auth()->user()->id;
+        $transaction_id=uniqid();
+        $price=$request->input('amt');
+        $menu_item_id = $request->input('menu_item_id');
+        $menu_item_id=$this->menu;
+        $total_amount=$request->input('tAmt');
+
+        //      orderfood::create([
+        //     'transaction_id'=>$refId,
+        //     'user_id'=>auth()->user()->id,
+        //     'menu_item_id'=>$menu_item_id,
+        //     'price'=>$price,
+        //     'total_amount'=>$total_amount,
+        // ]);
+
+        // dd($request->all());
+        // return 'Payment Successful with Reference ID: ' . $refId.'</br>' .$user.'<br>' .$transaction_id .'</br>'.$price.'</br>'.$menu_item_id .'</br>'.$total_amount .'</br>';
+        return 'Payment Successful with Reference ID: ' . $refId . '</br>'
+        . 'User ID: ' . $user . '<br>'
+        . 'Transaction ID: ' . $transaction_id . '</br>'
+        . 'Amount: ' . $price . '</br>'
+        . 'Menu Item ID: ' . $menu_item_id . '</br>'
+        . 'Total Amount: ' . $total_amount . '</br>';
         // return redirect('/')
     }
 
